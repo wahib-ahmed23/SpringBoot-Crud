0ddcbe2024319c2272afa0fb399bf9a4d1d8e62e
wahib-ahmed23 <94791133+wahib-ahmed23@users.noreply.github.com>
2023-05-03 11:48:10 +0530

    Springcurd example curd

---
+++ src/test/java/com/example/SpringCURD/SpringCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCURD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
